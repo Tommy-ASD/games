396f57e5d2b34dc5874318bccaa12182f3e01931
Tommy-ASD <82094450+Tommy-ASD@users.noreply.github.com>
2022-05-02 08:12:58 +0200

    changed stuff

--- wordle/words/find.py
+++ wordle/words/find.py
@@ -4,17 +4,17 @@ from wordleWords import fiveLetterWords
 def main():
     for i in range(len(fiveLetterWords)):
         if (
-            "u" in fiveLetterWords[i]
-            and "g" in fiveLetterWords[i]
-            and "e" in fiveLetterWords[i][4]
-            and "g" not in fiveLetterWords[i][4]
-            and "u" not in fiveLetterWords[i][2]
-            and "g" not in fiveLetterWords[i][2]
-            and "u" not in fiveLetterWords[i][3]
-            and "l" not in fiveLetterWords[i]
-            and "f" not in fiveLetterWords[i]
-            and "v" not in fiveLetterWords[i]
+            "s" in fiveLetterWords[i][2]
+            and "t" in fiveLetterWords[i][3]
+            and "y" in fiveLetterWords[i][4]
+            and "w" not in fiveLetterWords[i]
+            and "o" not in fiveLetterWords[i]
             and "a" not in fiveLetterWords[i]
+            and "m" not in fiveLetterWords[i]
+            and "b" not in fiveLetterWords[i]
+            and "u" not in fiveLetterWords[i]
+            and "n" not in fiveLetterWords[i]
+            and "i" not in fiveLetterWords[i]
         ):
             print(fiveLetterWords[i])
 
