10f07a79d3de4d5a4079fab4eaadcb2a2bfd7d98
Tommy-ASD <82094450+Tommy-ASD@users.noreply.github.com>
2022-05-05 12:10:49 +0200

    added getNeighbors to template

--- 2d/template.py
+++ 2d/template.py
@@ -34,16 +34,59 @@ class template:
                 currentX = 0
         print(msg)
 
-    def checkAbove(self, index):
+    def getNeighbors(self, index):
+        if self.field[index]["display"] == "b":
+            return
+        # Direction available
+        aboveA = False
+        belowA = False
+        leftA = True
+        rightA = True
+        # index - length is the one exactly above. if it does not exist, do not add the space there
+        if index - self.length >= 0:
+            aboveA = True
+        # same as previous, except index + length is exactly the one below
+        if index + self.length < self.spaces:
+            belowA = True
+        # example:
+        # length = 10
+        # length - 1 = 9
+        # if index % 10 returns 9, that means you are at the right edge of the grid
+        # if index % 10 returns 0, that means you are at the left edge
+        if index % self.length == self.length - 1:
+            rightA = False
+        if index % self.length == 0:
+            leftA = False
+        # if you remove/add length, you get the one exactly above
         above = index - self.length
-        if self.field[above]["state"] == "b":
-            self.field[index]["neighbors"] += 1
-        print(self.field[index]["neighbors"])
-        pass
+        below = index + self.length
+        # if you add/remove 1, you get the one to the right/left
+        left = index - 1
+        right = index + 1
+        # same logic as previous applies here, just more than once
+        aboveLeft = above - 1
+        aboveRight = above + 1
+        belowLeft = below - 1
+        belowRight = below + 1
+        # optimizable
+        # use arrays?
+        return (
+            above,
+            below,
+            left,
+            right,
+            aboveLeft,
+            aboveRight,
+            belowLeft,
+            belowRight,
+            aboveA,
+            belowA,
+            leftA,
+            rightA,
+        )
 
 
 temp = template()
 temp.createField()
 temp.generateObjects()
 temp.viewField()
-temp.checkAbove(19)
