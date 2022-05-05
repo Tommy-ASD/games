ecc8c54b1e88644672d7a54a5e9e923a542cf626
Tommy-ASD <82094450+Tommy-ASD@users.noreply.github.com>
2022-05-05 23:36:52 +0200

    removed unused code

--- 2d/tiktak.py
+++ 2d/tiktak.py
@@ -1,6 +1,3 @@
-import random
-
-
 class template:
     def __init__(self):
         self.player = False
@@ -33,57 +30,6 @@ class template:
             self.player = not self.player
         else:
             print("That index is already played")
-
-    def getNeighbors(self, index):
-        if self.field[index]["display"] == "b":
-            return
-        # Direction available
-        aboveA = False
-        belowA = False
-        leftA = True
-        rightA = True
-        # index - length is the one exactly above. if it does not exist, do not add the space there
-        if index - self.length >= 0:
-            aboveA = True
-        # same as previous, except index + length is exactly the one below
-        if index + self.length < self.spaces:
-            belowA = True
-        # example:
-        # length = 10
-        # length - 1 = 9
-        # if index % 10 returns 9, that means you are at the right edge of the grid
-        # if index % 10 returns 0, that means you are at the left edge
-        if index % self.length == self.length - 1:
-            rightA = False
-        if index % self.length == 0:
-            leftA = False
-        # if you remove/add length, you get the one exactly above
-        above = index - self.length
-        below = index + self.length
-        # if you add/remove 1, you get the one to the right/left
-        left = index - 1
-        right = index + 1
-        # same logic as previous applies here, just more than once
-        aboveLeft = above - 1
-        aboveRight = above + 1
-        belowLeft = below - 1
-        belowRight = below + 1
-        # optimizable
-        # use arrays?
-        return (
-            above,
-            below,
-            left,
-            right,
-            aboveLeft,
-            aboveRight,
-            belowLeft,
-            belowRight,
-            aboveA,
-            belowA,
-            leftA,
-            rightA,
-        )
 
     def convertToIndex(self, x, y):
         # input y: 10
